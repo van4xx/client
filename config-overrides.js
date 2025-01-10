94577ef8e19a042074cf934cc6d28f71c87e1e06
Van4xx <van200529ter@gmail.com>
2025-01-11 00:28:30 +0300

    add server part 32

--- config-overrides.js
+++ config-overrides.js
@@ -4,23 +4,38 @@ module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
     process: require.resolve("process/browser"),
-    buffer: require.resolve("buffer/"),
-    util: require.resolve("util/"),
+    buffer: require.resolve("buffer"),
+    util: require.resolve("util"),
     stream: require.resolve("stream-browserify"),
     crypto: require.resolve("crypto-browserify"),
-    assert: require.resolve("assert/"),
+    assert: require.resolve("assert"),
     http: require.resolve("stream-http"),
     https: require.resolve("https-browserify"),
     os: require.resolve("os-browserify/browser"),
-    url: require.resolve("url/"),
-    zlib: require.resolve("browserify-zlib")
+    url: require.resolve("url"),
+    zlib: require.resolve("browserify-zlib"),
+    path: false,
+    fs: false
   };
 
   config.plugins = [
     ...config.plugins,
     new webpack.ProvidePlugin({
-      process: ["process/browser"],
+      process: "process/browser",
       Buffer: ["buffer", "Buffer"]
+    }),
+    new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+      const mod = resource.request.replace(/^node:/, "");
+      switch (mod) {
+        case "buffer":
+          resource.request = "buffer";
+          break;
+        case "stream":
+          resource.request = "stream-browserify";
+          break;
+        default:
+          break;
+      }
     })
   ];
 
